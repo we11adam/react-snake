fb33846aebfb6820c63b12c0378df431561f1635
Adam L <skyros@gmail.com>
2020-02-20 21:05:22 +0800

    Updates

---
+++ precache-manifest.1f06d2d94f47808cb526e6f0892b036e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d419fdfd9acdf5afcc534d634d7147e3",
+    "url": "/react-snake/index.html"
+  },
+  {
+    "revision": "fd33a9ce2270b2a3f0c5",
+    "url": "/react-snake/static/css/main.3970e8e9.chunk.css"
+  },
+  {
+    "revision": "5e884a959339ab3ef126",
+    "url": "/react-snake/static/js/2.9b28f495.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-snake/static/js/2.9b28f495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd33a9ce2270b2a3f0c5",
+    "url": "/react-snake/static/js/main.b64cd60a.chunk.js"
+  },
+  {
+    "revision": "a38f28d4e9edb0674a00",
+    "url": "/react-snake/static/js/runtime-main.12ba3899.js"
+  }
+]);
